f66c47ecf4a60a2001bf31723740cb4e8b90eb14
Leon16x8 <leonardo.vieira160802@gmail.com>
2023-02-24 22:07:07 -0300

    Adicionando Fotos

--- PHP/transacoes.php
+++ PHP/transacoes.php
@@ -21,6 +21,7 @@ $cadd = new Inserir;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Histórico de Compras</title>
+    <link rel="shortcut icon type" type="image/x-icon" href="../Fotos/transa.png"/>
     <!--Estilos Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
